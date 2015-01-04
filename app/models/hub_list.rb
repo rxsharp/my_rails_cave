54496d25d56a3452561ff4d472a336a8853e23f2
Prelang Builder <builder@prelang.com>
2015-01-04 16:21:39 +0000

    Defined validation for attribute 'codepen' on model 'HubList'

--- app/models/hub_list.rb
+++ app/models/hub_list.rb
@@ -2,4 +2,5 @@ class HubList < ActiveRecord::Base
   belongs_to :user
   validates_formatting_of :github, using: :url
   validates_formatting_of :bitbucket, using: :url
+  validates_formatting_of :codepen, using: :url
 end
